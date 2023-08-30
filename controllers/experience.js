3495a8d1c7accd769b47a8d474232dd3572e15be
John9135 <palacioalexander5@gmail.com>
2023-08-30 14:25:22 -0500

    update: creation of function update for experience

--- controllers/experience.js
+++ controllers/experience.js
@@ -18,6 +18,35 @@ const controllerExperience = {
         } catch (error) {
             return res.status(500).json({ msg: error.message })
         }
+    },
+
+    getExperience: async (req, res) => {
+        try {
+            const experience = await Experience.find()
+            res.json({ experience })
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+
+    updateExperiencie: async (req, res) => {
+        try {
+            const { id } = req.params
+            const company = req.body.company
+            const position = req.body.position
+            const duration_experience = req.body.duration_experience
+            const description_position = req.body.description_position
+
+            await Experience.findByIdAndUpdate(id, {
+                company: company,
+                position: position,
+                duration_experience: duration_experience,
+                description_position: description_position
+            })
+            res.json({ msg: 'Update experience' })
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
     }
 }
 
--- routers/experience.js
+++ routers/experience.js
@@ -5,5 +5,7 @@ const controllerExperience = require('../controllers/experience')
 const router = express.Router()
 
 router.post('/create', controllerExperience.create)
+router.get('/', controllerExperience.getExperience)
+router.patch('/update/:id', controllerExperience.updateExperiencie)
 
 module.exports = router
